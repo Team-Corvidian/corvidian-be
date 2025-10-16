42927e17964dab8964d2e950c860dd82d25f7930
gracekarinn <gracekarina12@gmail.com>
2025-10-16 23:22:20 +0700

    feat: add article model

---
+++ main/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.utils.text import slugify
+from ckeditor.fields import RichTextField
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, blank=True)
+    author = models.CharField(max_length=100)
+    published_at = models.DateField()
+    cover_image = models.ImageField(upload_to='wawasan/covers/', blank=True, null=True)
+    content = RichTextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
