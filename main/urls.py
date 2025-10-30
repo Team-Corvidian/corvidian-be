0a45fe2238c7b2541bf639152e59e36bc614a179
Grace Karina <gracekarina12@gmail.com>
2025-10-30 12:10:21 +0700

    feat: add consultation and newsletter subscription endpoints to urls

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ArticleViewSet, ArticleDetailBySlugView
+from .views import ArticleViewSet, ArticleDetailBySlugView, ConsultationSubmitView, NewsletterSubscribeView
 
 router = DefaultRouter()
-router.register(r'wawasan', ArticleViewSet)
+router.register(r'articles', ArticleViewSet, basename='articles')
 
 urlpatterns = [
     path('', include(router.urls)),
     path('wawasan/slug/<slug:slug>/', ArticleDetailBySlugView.as_view(), name='article-detail-by-slug'),
+    path("consultation/submit/", ConsultationSubmitView.as_view(), name="consultation-submit"),
+    path("subscribe/", NewsletterSubscribeView.as_view()),
 ]
