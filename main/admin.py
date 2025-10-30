bc54f8c6b465642b99019c16a6f839e4c0cfe26a
Grace Karina <gracekarina12@gmail.com>
2025-10-30 12:09:38 +0700

    feat: register new models in admin

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, ConsultationLead, NewsletterSubscriber
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
@@ -7,3 +7,15 @@ class ArticleAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     search_fields = ('title', 'author', 'content')
     list_filter = ('published_at',)
+
+@admin.register(ConsultationLead)
+class ConsultationLeadAdmin(admin.ModelAdmin):
+    list_display = ("name", "email", "phone", "company", "created_at")
+    search_fields = ("name", "email", "company", "phone")
+    list_filter = ("created_at",)
+
+@admin.register(NewsletterSubscriber)
+class NewsletterSubscriberAdmin(admin.ModelAdmin):
+    list_display = ("email", "source", "created_at")
+    search_fields = ("email", "source")
+    list_filter = ("created_at", "source")
