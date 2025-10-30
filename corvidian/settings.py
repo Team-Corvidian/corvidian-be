5fc88ea428b3aef27197a58b244dc7f5d31f19fc
Grace Karina <gracekarina12@gmail.com>
2025-10-30 12:09:18 +0700

    feat: configure email backend and add consultation contact settings

--- corvidian/settings.py
+++ corvidian/settings.py
@@ -133,3 +133,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+CONSULTATION_RECEIVER_EMAIL = os.getenv("CONSULTATION_RECEIVER_EMAIL")
+CONSULTATION_WHATSAPP = os.getenv("CONSULTATION_WHATSAPP")
+
+
