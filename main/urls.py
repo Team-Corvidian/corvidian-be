8376b633c37fa7060907015735bffa0b147bc82d
gracekarinn <gracekarina12@gmail.com>
2025-11-03 18:25:00 +0700

    refactor: remove unused router

--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
 from .views import ArticleViewSet, ArticleDetailBySlugView, ConsultationSubmitView, NewsletterSubscribeView
 
-router = DefaultRouter()
-router.register(r'articles', ArticleViewSet, basename='articles')
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('wawasan/', ArticleViewSet.as_view({'get': 'list'}), name='article-list'),
     path('wawasan/slug/<slug:slug>/', ArticleDetailBySlugView.as_view(), name='article-detail-by-slug'),
     path("consultation/submit/", ConsultationSubmitView.as_view(), name="consultation-submit"),
     path("subscribe/", NewsletterSubscribeView.as_view()),
